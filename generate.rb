459c21692296ca67c3a67dd2ac06b3efc32780ec
Dan Stutzman <dan@danstutzman.com>
2015-09-07 21:19:09 -0600

    Add past tense, and try to stop question words from showing up in sentences that should be simple.  Also remove redundant agent pronouns and add some missing accent marks.

--- generate.rb
+++ generate.rb
@@ -24,28 +24,23 @@ end
 
 def choose_agent(features)
   choices = [
-    ['I',         'yo', {gender:'?', person:1, number:'S', question:false}],
     ['I',          nil, {gender:'?', person:1, number:'S', question:false}],
-    ['you',       'tu', {gender:'?', person:2, number:'S', question:false}],
     ['you',        nil, {gender:'?', person:2, number:'S', question:false}],
-    ['he',        'el', {gender:'M', person:3, number:'S', question:false}],
+    ['he',        'él', {gender:'M', person:3, number:'S', question:false}],
     ['she',     'ella', {gender:'F', person:3, number:'S', question:false}],
     ['it',        'el', {gender:'N', person:3, number:'S', question:false}],
     ['it',      'ella', {gender:'N', person:3, number:'S', question:false}],
-    ['we',  'nosotros', {gender:'?', person:1, number:'P', question:false}],
     ['we',         nil, {gender:'?', person:1, number:'P', question:false}],
-    ['they',   'ellos', {gender:'?', person:3, number:'P', question:false}],
-    ['they',   'ellas', {gender:'?', person:3, number:'P', question:false}],
     ['they',       nil, {gender:'?', person:3, number:'P', question:false}],
   ]
-  if features[:question] != true # if it's not already a question
+  if features[:question].nil? # if it's not already a question
     choices += [
-      ['who',    'quien', {gender:'N', person:3, number:'S', question:true}],
+      ['who',    'quién', {gender:'N', person:3, number:'S', question:true}],
     ]
   end
   l1, l2, agent_features = one_of(choices)
-  if features[:question]
-    agent_features[:question] = true # pass along question:true down the sentence
+  if !features[:question].nil?
+    agent_features[:question] = features[:question]
   end
   [[l1], l2 ? [l2] : [], agent_features]
 end
@@ -87,8 +82,8 @@ def choose_object(features)
   else
     choices = []
     if features[:actor] != false
-      if !features[:question]
-        choices.push [['whom'], ['quien'], {question:true}]
+      if features[:question].nil?
+        choices.push [['whom'], ['quién'], {question:true}]
       end
       choices += [
         [['Bill'], ['Bill'], {}],
@@ -105,55 +100,128 @@ def choose_object(features)
   end
 end
 
+def add_d(l1)
+  l1.gsub! /y$/, 'ie'
+  case l1
+    when 'give' then 'gave'
+    when 'know' then 'knew'
+    else l1.sub(/([tdn])$/, '\1e') + 'd'
+  end
+end
+
+def add_s(l1)
+  l1.gsub! /y$/, 'ie'
+  l1 + 's'
+end
+
 def conjugate_l1_verb(l1, features)
-  if features[:infinitive]
+  if features[:tense] == 'infinitive'
     ['to', l1]
   elsif features[:needs_auxiliary]
-    if features[:person] == 3 && features[:number] == 'S'
-      ['does', l1]
-    else
-      ['do', l1]
+    case features[:tense]
+      when 'past'
+        ['did', l1]
+      when 'present'
+        if features[:person] == 3 && features[:number] == 'S'
+          ['does', l1]
+        else
+          ['do', l1]
+        end
+      else raise "Don't know how to conjugate #{l1} for #{features}"
     end
   elsif features[:person] == 3 && features[:number] == 'S'
-    l1.gsub! /y$/, 'ie'
-    [l1 + 's']
+    case features[:tense]
+      when 'past'    then [add_d(l1)]
+      when 'present' then [add_s(l1)]
+      else raise "Don't know how to conjugate #{l1} for #{features}"
+    end
   else
-    [l1]
+    case features[:tense]
+      when 'past' then [add_d(l1)]
+      when 'present' then [l1]
+      else raise "Don't know how to conjugate #{l1} for #{features}"
+    end
   end
 end
 
 def conjugate_l2_verb(l2, features)
-  selector = [features[:person], features[:number], l2[-2..-1]]
-  if features[:infinitive]
-    return l2
-  elsif l2 == 'dar' && selector == [1, 'S', 'ar']
-    'doy'
-  else
-    l2_base = l2[0...-2]
-    unless features[:person] == 1 && features[:number] == 'P'
-      l2_base = case l2
-        when 'querer' then 'quier'
-        when 'probar' then 'prueb'
-        else l2_base
-      end
+  case features[:tense]
+  when 'infinitive' then l2
+  when 'past'
+    l2_base, suffix = case l2
+      when 'querer' then ['quis', '**']
+      when 'dar'    then ['d', 'ir']
+      when 'saber'  then ['sup', '**']
+      when 'pedir'  then features[:person] == 3 ? ['pid', 'ir'] : ['ped', 'ir']
+      else               [l2[0...-2], l2[-2..-1]]
     end
+    selector = [features[:person], features[:number], suffix]
     l2_conjugated = case selector
-      when [1, 'S', 'ar'] then l2_base + 'o'
-      when [2, 'S', 'ar'] then l2_base + 'as'
-      when [3, 'S', 'ar'] then l2_base + 'a'
+      when [1, 'S', 'ar'] then l2_base + 'é'
+      when [2, 'S', 'ar'] then l2_base + 'aste'
+      when [3, 'S', 'ar'] then l2_base + 'ó'
       when [1, 'P', 'ar'] then l2_base + 'amos'
-      when [3, 'P', 'ar'] then l2_base + 'an'
-      when [1, 'S', 'er'] then l2_base + 'o'
-      when [2, 'S', 'er'] then l2_base + 'es'
-      when [3, 'S', 'er'] then l2_base + 'e'
-      when [1, 'P', 'er'] then l2_base + 'emos'
-      when [3, 'P', 'er'] then l2_base + 'en'
+      when [3, 'P', 'ar'] then l2_base + 'aron'
+      when [1, 'S', 'er'] then l2_base + 'í'
+      when [2, 'S', 'er'] then l2_base + 'iste'
+      when [3, 'S', 'er'] then l2_base + 'ió'
+      when [1, 'P', 'er'] then l2_base + 'imos'
+      when [3, 'P', 'er'] then l2_base + 'ieron'
+      when [1, 'S', 'ir'] then l2_base + 'í'
+      when [2, 'S', 'ir'] then l2_base + 'iste'
+      when [3, 'S', 'ir'] then l2_base + 'ió'
+      when [1, 'P', 'ir'] then l2_base + 'imos'
+      when [3, 'P', 'ir'] then l2_base + 'ieron'
+      when [1, 'S', '**'] then l2_base + 'e'
+      when [2, 'S', '**'] then l2_base + 'iste'
+      when [3, 'S', '**'] then l2_base + 'o'
+      when [1, 'P', '**'] then l2_base + 'imos'
+      when [3, 'P', '**'] then l2_base + 'ieron'
       else raise "Can't handle #{selector} for verb #{l2}"
     end
+  when 'present'
+    l2_base, suffix = [l2[0...-2], l2[-2..-1]]
+    selector = [features[:person], features[:number], suffix]
+    if l2 == 'dar' && selector == [1, 'S', 'ar']
+      'doy'
+    elsif l2 == 'saber' && selector == [1, 'S', 'er']
+      'sé'
+    else
+      unless features[:person] == 1 && features[:number] == 'P'
+        l2_base = case l2
+          when 'querer' then 'quier'
+          when 'probar' then 'prueb'
+          when 'pedir'  then 'pid'
+          else l2_base
+        end
+      end
+      l2_conjugated = case selector
+        when [1, 'S', 'ar'] then l2_base + 'o'
+        when [2, 'S', 'ar'] then l2_base + 'as'
+        when [3, 'S', 'ar'] then l2_base + 'a'
+        when [1, 'P', 'ar'] then l2_base + 'amos'
+        when [3, 'P', 'ar'] then l2_base + 'an'
+        when [1, 'S', 'er'] then l2_base + 'o'
+        when [2, 'S', 'er'] then l2_base + 'es'
+        when [3, 'S', 'er'] then l2_base + 'e'
+        when [1, 'P', 'er'] then l2_base + 'emos'
+        when [3, 'P', 'er'] then l2_base + 'en'
+        when [1, 'S', 'ir'] then l2_base + 'o'
+        when [2, 'S', 'ir'] then l2_base + 'es'
+        when [3, 'S', 'ir'] then l2_base + 'e'
+        when [1, 'P', 'ir'] then l2_base + 'imos'
+        when [3, 'P', 'ir'] then l2_base + 'en'
+        else raise "Can't handle #{selector} for verb #{l2}"
+      end
+    end
+    else raise "Don't know how to conjugate #{l2} for #{features}"
   end
 end
 
 def choose_vp(features)
+  if features[:tense].nil?
+    features[:tense] = one_of(['past', 'present'])
+  end
   case rand(10)
     when 0
       l1, l2 = one_of([
@@ -162,7 +230,7 @@ def choose_vp(features)
       ])
 
       object_l1, object_l2, object_features =
-        choose_vp(features.merge({infinitive:true}))
+        choose_vp(features.merge({tense:'infinitive'}))
     when 1
       l1, l2 = one_of([
         ['give', 'dar'],
@@ -177,18 +245,19 @@ def choose_vp(features)
       object_features = receiver_features
 
       if rand(2) == 0
-        vp_l1, vp_l2, vp_features = choose_vp(features.merge({infinitive:true}))
+        vp_l1, vp_l2, vp_features = choose_vp(features.merge({tense:'infinitive'}))
         object_l1 += vp_l1
         object_l2 += ['para'] + vp_l2
-        if vp_features[:question]
-          object_features[:question] = true
+        if !vp_features[:question].nil?
+          object_features[:question] = vp_features[:question]
         end
       end
 
     else
       l1, l2, actor = one_of([
-        ['need', 'necesitar', false],
-        ['want', 'querer',    false],
+        ['need',    'necesitar', false],
+        ['want',    'querer',    false],
+        ['request', 'pedir',     false],
       ])
 
       if actor != false
@@ -214,7 +283,7 @@ end
 
 def choose_sentence(must_be_simple)
   if must_be_simple || rand(2) == 0
-    agent_l1, agent_l2, agent_features = choose_agent({})
+    agent_l1, agent_l2, agent_features = choose_agent({question: false})
     vp_l1, vp_l2, vp_features = choose_vp(agent_features)
     features = {question: agent_features[:question] || vp_features[:question]}
     [agent_l1 + vp_l1, agent_l2 + vp_l2, features]
@@ -245,6 +314,7 @@ def choose_sentence(must_be_simple)
       [l1, l2, features]
     when 2 # Agent knows that S
       agent_l1, agent_l2, agent_features = choose_agent({})
+      agent_features[:tense] = one_of(['past', 'present'])
       vp_l1, vp_l2, vp_features = 'know', 'saber', {}
       vp_l1 = conjugate_l1_verb(vp_l1, agent_features)
       vp_l2 = conjugate_l2_verb(vp_l2, agent_features)
