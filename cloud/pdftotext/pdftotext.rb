abe9712309201b30ba63dbd0d00b0a99a0b7b934
Dan Stutzman <dan@danstutzman.com>
2015-12-01 08:47:22 -0600

    Now can parse most of the PDFs of any level.

--- cloud/pdftotext/pdftotext.rb
+++ cloud/pdftotext/pdftotext.rb
@@ -2,24 +2,31 @@ require 'citrus'
 
 Citrus.load('calc.citrus')
 
-base = "/mnt/transmission-daemon/downloads/Chinesepod Collection/ChinesePod Newbie/"
-Dir.entries(base).each do |path|
-  next if path == '.' || path == '..'
-  next unless path.match(/.pdf$/)
-  next if [
-    'A0784 - The Fourth Tone.pdf',
-    'A0775 - The Second Tone.pdf',
-  ].include?(path)
-  puts path
-
-  `pdftotext "#{base}/#{path}" out.txt | head -1`
-  out = File.read('out.txt')
-  out.gsub! "hang\nout", 'hang out'
-  out.gsub! "name\ncard.", 'name card.'
-  out.gsub! "sour\npork", 'sour pork'
+ARGV.each do |path|
+  next if path.end_with?('B0530.txt')
+  next if path.end_with?('B0679.txt')
+  next if path.end_with?('B0698.txt')
+  next if path.end_with?('B0702.txt')
+  next if path.end_with?('C0418.txt')
+  next if path.end_with?('C0470.txt')
+  next if path.end_with?('C0529.txt')
+  next if path.end_with?('C0589.txt')
+  out = File.read(path)
+#  out.gsub! "hang\nout", 'hang out'
+#  out.gsub! "name\ncard.", 'name card.'
+#  out.gsub! "sour\npork", 'sour pork'
   out.gsub! "doin’\n’?", "doin’?"
   out.gsub! "\x75\xcc\x88\x0a\x75\xcc\x8c", "üǔ"
-  out.strip!
-  Calc.parse(out).value
+  out.gsub! "Broken-Hearted\nSon", 'Broken-Hearted Son'
+  out.gsub! "Peter\n\n(C0566)\n\n，欢迎参观我们工厂！\n\n",
+    "(C0566)\n\nPeter，欢迎参观我们工厂！\n"
+
+#  out.strip!
+  begin
+    Calc.parse(out).value
+    puts "  #{path}"
+  rescue Exception => e
+    puts "* #{path}"
+  end
 end
 
