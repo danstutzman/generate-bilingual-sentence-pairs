1d0e87535faf49a9bcc5bd032d5658fca10ecbfc
Dan Stutzman <dan@danstutzman.com>
2016-10-27 12:59:09 -0600

    Move pdftotext to convert-chinesepod-pdf-to-text bitbucket repo

--- cloud/pdftotext/go.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -ex
-pdftotext "/mnt/transmission-daemon/downloads/Chinesepod Collection/ChinesePod Newbie/A0001 - Intro 1 Good morning.pdf" out.txt
-pdftotext "/mnt/transmission-daemon/downloads/Chinesepod Collection/ChinesePod Newbie/A0002 - Intro 2 What's your name.pdf" out.txt
