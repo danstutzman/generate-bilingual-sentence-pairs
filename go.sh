ae9840e9e7ed6f8acf16e3237618555e9cc4fa47
Dan Stutzman <dan@danstutzman.com>
2016-10-27 21:24:30 -0600

    Clean build directory before running mocha tests

--- go.sh
+++ go.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -ex
 node_modules/.bin/flow
+rm -rf build
 node_modules/.bin/flow-remove-types --out-dir build `find src -name '*.js'` `find test -name '*.js'`
 node_modules/.bin/mocha build/test
