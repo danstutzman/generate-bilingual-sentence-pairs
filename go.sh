5972678e1bd245f7732088dc09c5abf15c7942c4
Dan Stutzman <dan@danstutzman.com>
2016-10-28 20:17:02 -0600

    Add jshint

--- go.sh
+++ go.sh
@@ -5,4 +5,5 @@ JS_FILES="$JS_FILES `find test -name *.js`"
 grep ";$" $JS_FILES && exit 1 # don't allow lines to end with semicolons
 rm -rf build
 node_modules/.bin/flow-remove-types --out-dir build $JS_FILES
+node_modules/.bin/jshint `find build -name '*.js'`
 node_modules/.bin/mocha build/test
