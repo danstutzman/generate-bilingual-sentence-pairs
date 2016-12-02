2700cb2bf6e8f95076ab65a4e6e9eaacc59e61d5
Dan Stutzman <dan@danstutzman.com>
2016-12-02 15:47:01 -0700

    Moved conjugate function from Translator to es/verbs/index.js

--- src/generate6/es/Translator.js
+++ src/generate6/es/Translator.js
@@ -5,47 +5,12 @@ import type { Conjugation } from './verbs'
 
 const { UniIClause } = require('../uni/uni_iclause')
 const { raise } = require('../raise')
-const RegularConjugation = require('./verbs/RegularConjugation')
-const regular_conjugation_pattern_table =
-  require('./verbs/regular_conjugation_pattern_table')
-const unique_conjugation_table = require('./verbs/unique_conjugation_table')
-const stem_change_table = require('./verbs/stem_change_table')
 const EsPronoun = require('./EsPronoun')
 const EsPronouns = require('./EsPronouns')
 const EsIClause = require('./EsIClause')
 const { UniNClause } = require('../uni/noun_phrases')
 const { NameNoun, EsNounClause } = require('./noun_phrases')
-
-function conjugate(infinitive:string, tense:Tense, person:Person, number:Number):
-    Conjugation {
-  const uniqueConjugations = unique_conjugation_table.find01(
-    infinitive, tense, person, number)
-  if (uniqueConjugations.length === 1) {
-    return uniqueConjugations[0]
-  }
-
-  const stemChanges = stem_change_table.find01(infinitive, tense)
-  if (stemChanges.length === 1) {
-    const patterns = regular_conjugation_pattern_table.find01(
-      infinitive, tense, person, number, true)
-    if (patterns.length === 1) {
-      return new stem_change_table.StemChangeConjugation(infinitive, stemChanges[0],
-        patterns[0])
-    } else {
-      throw new Error(`Can't find RegularConjugationPattern for stem-changing
-        ${infinitive}.${tense}.${person}.${number}`)
-    }
-  }
-
-  const regularPatterns = regular_conjugation_pattern_table.find01(
-    infinitive, tense, person, number, false)
-  if (regularPatterns.length === 1) {
-    return new RegularConjugation({ infinitive, pattern:regularPatterns[0] })
-  } else {
-    throw new Error(`Can't find UniqueConjugation or RegularConjugation for
-      ${infinitive}.${tense}.${person}.${number}`)
-  }
-}
+const { conjugate } = require('./verbs')
 
 class Translator {
   tense:                  Tense
--- src/generate6/es/verbs/index.js
+++ src/generate6/es/verbs/index.js
@@ -1,7 +1,44 @@
 // @flow
+import type { Person, Tense, Number, PreferredPronouns } from '../types'
 
 const RegularConjugation = require('./RegularConjugation')
-const { UniqueConjugation } = require('./unique_conjugation_table')
-const { StemChangeConjugation } = require('./stem_change_table')
+const regular_conjugation_pattern_table = require('./regular_conjugation_pattern_table')
+const unique_conjugation_table = require('./unique_conjugation_table')
+const stem_change_table = require('./stem_change_table')
 
-export type Conjugation = RegularConjugation | UniqueConjugation | StemChangeConjugation
+export type Conjugation = RegularConjugation |
+  unique_conjugation_table.UniqueConjugation |
+  stem_change_table.StemChangeConjugation
+
+function conjugate(infinitive:string, tense:Tense, person:Person, number:Number):
+    Conjugation {
+  const uniqueConjugations = unique_conjugation_table.find01(
+    infinitive, tense, person, number)
+  if (uniqueConjugations.length === 1) {
+    return uniqueConjugations[0]
+  }
+
+  const stemChanges = stem_change_table.find01(infinitive, tense)
+  if (stemChanges.length === 1) {
+    const patterns = regular_conjugation_pattern_table.find01(
+      infinitive, tense, person, number, true)
+    if (patterns.length === 1) {
+      return new stem_change_table.StemChangeConjugation(infinitive, stemChanges[0],
+        patterns[0])
+    } else {
+      throw new Error(`Can't find RegularConjugationPattern for stem-changing
+        ${infinitive}.${tense}.${person}.${number}`)
+    }
+  }
+
+  const regularPatterns = regular_conjugation_pattern_table.find01(
+    infinitive, tense, person, number, false)
+  if (regularPatterns.length === 1) {
+    return new RegularConjugation({ infinitive, pattern:regularPatterns[0] })
+  } else {
+    throw new Error(`Can't find UniqueConjugation or RegularConjugation for
+      ${infinitive}.${tense}.${person}.${number}`)
+  }
+}
+
+module.exports = { conjugate }
